f7687da0e1d5118fbff01d325321c70ccb7869a4
Jacek Czapla <jczapla@asit.pl>
2018-04-11 14:21:30 +0200

    zapamiętanie ustawienia logowania

--- StoreBtnLocationExample/Form1.Designer.cs
+++ StoreBtnLocationExample/Form1.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(256, 185);
+            this.button2.Location = new System.Drawing.Point(235, 185);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -51,6 +52,26 @@
             this.textBox1.Size = new System.Drawing.Size(159, 20);
             this.textBox1.TabIndex = 3;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Wpisz hasło";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(59, 215);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Zapamietaj";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // button1
             // 
             this.button1.DataBindings.Add(new System.Windows.Forms.Binding("Enabled", global::StoreBtnLocationExample.Properties.Settings.Default, "Button1Enable", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
@@ -62,27 +83,19 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 172);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Wpisz hasło";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(559, 370);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
-            
+            this.Activated += new System.EventHandler(this.Form1_Activated);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,6 +107,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- StoreBtnLocationExample/Form1.cs
+++ StoreBtnLocationExample/Form1.cs
@@ -19,6 +19,8 @@ namespace StoreBtnLocationExample
 
         private void button2_Click(object sender, EventArgs e)
         {
+            
+
             // odczyt hasła z settings
             string pass = Properties.Settings.Default.Password;
             //sprawdzenie hasa
@@ -33,6 +35,20 @@ namespace StoreBtnLocationExample
             }
             
             
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            bool RememberMe = Properties.Settings.Default.RememberMe;
+            if (RememberMe) {
+                textBox1.Text = Properties.Settings.Default.Password;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.RememberMe = checkBox1.Checked;
+            Properties.Settings.Default.Save();
         }
     }
 }
